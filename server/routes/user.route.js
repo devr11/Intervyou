f73ed269d3d7e5f4ec436703c15e6e6ef63a780d
Dev <rastogi.db1@gmail.com>
2026-06-25 14:29:52 +0530

    update routes

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,7 +2,6 @@ import express from "express"
 import isAuth from "../middlewares/isAuth.js"
 import { getCurrentUser } from "../controllers/user.controller.js"
 
-
 const userRouter = express.Router()
 
 userRouter.get("/current-user",isAuth,getCurrentUser)
